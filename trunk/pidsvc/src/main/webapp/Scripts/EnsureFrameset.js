95e38523b7d7e657a3c145d49a0028d8eaecaa1b
PavelGolodoniuc <PavelGolodoniuc@7d86ec14-9586-4400-877e-29b173b7fd00>
2012-10-24 05:01:13 +0000

    Fixed frameset handling logic.

--- trunk/pidsvc/src/main/webapp/Scripts/EnsureFrameset.js
+++ trunk/pidsvc/src/main/webapp/Scripts/EnsureFrameset.js
@@ -1,6 +1,10 @@
 ﻿var G_APPLICATION_PATH = "/pidsvc/"; 
-$J(function ()
-{
-	if (top.location.href == this.location.href)
-		location.href = G_APPLICATION_PATH + "?page=" + location.pathname;
+$J(function() {
+	if (location.href.match(/main_frameset.html$/gi))
+	{
+		var page = top.location.href.getQueryParam("page");
+		$J("#fraContent").attr("src", (page && page !== true ? decodeURIComponent(page) : G_APPLICATION_PATH + "mappings.html"));
+	}
+	else if (top.location.href == this.location.href)
+		location.href = G_APPLICATION_PATH + "?page=" + encodeURIComponent(location.pathname);
 });
