dbb81a9af9cf8d7c27b4e5f2ba5831ca5e9f6914
PavelGolodoniuc <pavel.golodoniuc@csiro.au>
2012-12-13 04:42:49 +0000

    Added manifest management logic.

--- pidsvc/src/main/java/csiro/pidsvc/core/Settings.java
+++ pidsvc/src/main/java/csiro/pidsvc/core/Settings.java
@@ -1,18 +1,32 @@
 package csiro.pidsvc.core;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+import java.util.jar.Manifest;
 
 public class Settings
 {
-	private static Settings _instance = null;
-	private Properties _properties = new Properties();
+	private static Settings			_instance = null;
+	protected Properties			_properties = new Properties();
+	protected Manifest				_manifest = new Manifest();
+	protected Map<String, String>	_serverProperties = new HashMap<String, String>(6);
 
 	private Settings()
 	{
 		try
 		{
-			_properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("../pidsvc.properties"));
+			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+			_properties.load(classLoader.getResourceAsStream("../pidsvc.properties"));
+			_manifest.read(classLoader.getResourceAsStream("../../META-INF/MANIFEST.MF"));
+
+			_serverProperties.put("serverJavaVersion", System.getProperty("java.version"));
+			_serverProperties.put("serverJavaVendor", System.getProperty("java.vendor"));
+            _serverProperties.put("javaHome", System.getProperty("java.home"));
+            _serverProperties.put("serverOsArch", System.getProperty("os.arch"));
+            _serverProperties.put("serverOsName", System.getProperty("os.name"));
+            _serverProperties.put("serverOsVersion", System.getProperty("os.version"));
 		}
 		catch (IOException e)
 		{
@@ -41,5 +55,20 @@ public class Settings
 	public String getProperty(String name)
 	{
 		return _properties.getProperty(name);
+	}
+
+	public Manifest getManifest()
+	{
+		return _manifest;
+	}
+
+	public String getManifestProperty(String name)
+	{
+		return _manifest.getMainAttributes().getValue(name);
+	}
+
+	public Map<String, String> getServerProperties()
+	{
+		return _serverProperties;
 	}
 }
