0400f5acd3c31508ed32091d28dc1f9f7c73369a
PavelGolodoniuc <pavel.golodoniuc@csiro.au>
2013-06-18 06:12:11 +0000

    DEBUG: Additional tracing.

--- pidsvc/src/main/java/csiro/pidsvc/mappingstore/Manager.java
+++ pidsvc/src/main/java/csiro/pidsvc/mappingstore/Manager.java
@@ -968,6 +968,11 @@ public class Manager
 	public String resolveLookupValue(String ns, String key)
 	{
 		LookupMapDescriptor lookupDescriptor = getLookupMapType(ns);
+		//---
+		_logger.trace("lookupDescriptor == null {}", lookupDescriptor == null);
+		if (lookupDescriptor != null)
+			//---
+			_logger.trace("lookupDescriptor.isStatic {}", lookupDescriptor.isStatic());
 		if (lookupDescriptor == null)
 			return null;
 
@@ -976,6 +981,10 @@ public class Manager
 			if (lookupDescriptor.isStatic())
 			{
 				String ret = getLookupValue(ns, key);
+				//---
+				_logger.trace("getLookupValue({}, {}) = {}", ns, key, ret);
+				//---
+				_logger.trace("lookupDescriptor.getDefaultValue = ", lookupDescriptor.getDefaultValue(key));
 				return ret == null ? lookupDescriptor.getDefaultValue(key) : ret;
 			}
 			else if (lookupDescriptor.isHttpResolver())
