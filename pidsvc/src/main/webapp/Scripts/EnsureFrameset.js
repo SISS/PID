42be61393bb2bdccdc59c807a9db8cee77c502f9
PavelGolodoniuc <pavel.golodoniuc@csiro.au>
2013-07-16 03:12:19 +0000

    Fixed hard-coded application path.

--- pidsvc/src/main/webapp/Scripts/EnsureFrameset.js
+++ pidsvc/src/main/webapp/Scripts/EnsureFrameset.js
@@ -8,13 +8,13 @@
  * License Agreement license, available at the root application directory.
  */
 
-var G_APPLICATION_PATH = "/pidsvc/"; 
 $J(function() {
-	if (location.href.match(/main_frameset.html$/gi))
+	var appPath = location.href.replace(/^.*(\/.+?\/)[^\/]*$/i, "$1");
+	if (location.href.match(/main_frameset.html$/i))
 	{
 		var page = top.location.href.getQueryParam("page");
-		$J("#fraContent").attr("src", (page && page !== true ? decodeURIComponent(page) : G_APPLICATION_PATH + "mappings.html"));
+		$J("#fraContent").attr("src", (page && page !== true ? decodeURIComponent(page) : appPath + "mappings.html"));
 	}
 	else if (top.location.href == this.location.href)
-		location.href = G_APPLICATION_PATH + "?page=" + encodeURIComponent(location.href);
+		location.href = appPath + "?page=" + encodeURIComponent(location.href);
 });
